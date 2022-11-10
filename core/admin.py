6450108c646cd43fd899cd8cde54f125e76d4cbf
shahnozahaydarova <shahnozahaydarova03@gmail.com>
2022-11-10 23:37:18 +0500

    new db

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Odam
+from .models import Gul
+
 admin.site.register(Odam)
+admin.site.register(Gul)
 
 # Register your models here.
--- core/models.py
+++ core/models.py
@@ -5,4 +5,8 @@ class Odam(models.Model):
     ism = models.CharField(max_length=15)
     familiya = models.CharField(max_length=18)
     yosh = models.IntegerField(default=16)
+
+class Gul(models.Model):
+    tur = models.CharField(max_length=14)
+    rang = models.CharField(max_length=8)
     
